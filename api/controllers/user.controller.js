f6055bb0a96781180d079b1efde8cfe763c805e5
Thisara <sasmithajayasinghe1@gmail.com>
2023-12-27 11:57:59 +0530

    c

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,20 @@
+import { errorHandler } from "../utils/error"
+import bcryptjs from 'bcryptjs'
 
 export const test = (req,res)=>{
     res.json({
         message: "Hello there !"
     })
+}
+
+export const updateUser = (req,res,next) =>{
+    if(req.user.id !== req.params.id) 
+    return next(errorHandler(401,'You can only update your own account'))
+    try{
+        if(req.body.password){
+            req.body.password = bcryptjs
+        }
+    }catch(error){
+        next(error)
+    }
 }
--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 import userRouter from './routes/user.routes.js'
 import authRouter from './routes/auth.route.js'
+import {cookieParser} from 'cookie-parser'
 
 
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -14,6 +15,7 @@ mongoose.connect(process.env.MONGO).then(()=>{
 
 
 const app = express()
+app.use(cookieParser())
 
 app.listen(3000, () =>{
     console.log('server running in port 3000');
